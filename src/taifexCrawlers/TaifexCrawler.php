35bbde87830e289cd7e9cd317a189bc06af3d984
Nick Tsai <myintaer@gmail.com>
2024-04-15 18:03:22 +0800

    Release version 1.3.0

--- src/taifexCrawlers/TaifexCrawler.php
+++ src/taifexCrawlers/TaifexCrawler.php
@@ -6,7 +6,7 @@ namespace yidas\twStockCrawler\taifexCrawlers;
  * Crawler - Source of Taifex 
  * 
  * @author  Nick Tsai
- * @since   1.2.0
+ * @since   1.3.0
  */
 class TaifexCrawler extends BaseCrawler
 {
